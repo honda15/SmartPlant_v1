1e7119f394ffae3685dcb8e13fc9220fefc98e8a
honda.lee <mylife4451@gmail.com>
2023-09-22 00:32:08 +0800

    program_v2

---
+++ app/src/main/java/com/example/smartplant_v1/Function_UI.java
@@ -0,0 +1,14 @@
+package com.example.smartplant_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Function_UI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_function_ui);
+    }
+}
